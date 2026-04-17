dee65cd7c3af954e9e9adea07875a1de22ea77d8
brung <brung@br.ibm.com>
2026-04-17 17:59:44 -0300

    alteracao para atender automaação

--- features/step-definitions/Usuarios.ts
+++ features/step-definitions/Usuarios.ts
@@ -11,6 +11,14 @@ Then('o campo email deve conter o email {string}', async function (email: string
     await usuariosPages.validarEmailUsuario(this, email);
 });
 
+When('eu envio uma requisição PATCH para {string} com dados para alterar usuario com o id {int}', async function (endpoint: string, id: number) {
+    await usuariosPages.alterarUsuario(this, endpoint, id);
+});
+
 When('envio uma requisição DELETE para {string} com o id {int}', async function (endpoint: string, id: number) {
     await usuariosPages.deletarUsuario(this, endpoint, id);
 })
+
+Then('o response deve conter os campos:', async function (dataTable) {
+    await usuariosPages.validarCamposUsuarios(this, dataTable);
+});
--- src/pages/UsuariosPages.ts
+++ src/pages/UsuariosPages.ts
@@ -47,4 +47,55 @@ export class UsuariosPages {
 
         return world.response;
     }
+
+    async alterarUsuario(world: any, endpoint: string, id: number) {
+        const authHeader = Api.token.startsWith('Bearer') ? Api.token : `Bearer ${Api.token}`;
+        
+        const payload = {
+            id: `${id}`,
+            name: "Bruno Nunes Garcia",
+            username: "brung",
+            email: "brung@rederecord.com.br",
+            active: true,
+            profileId: 1,
+            departmentId: 1
+        };
+
+        const cineflixContext: APIRequestContext = await request.newContext({
+            baseURL: process.env.BASE_URL,
+            extraHTTPHeaders: {
+                'Authorization': authHeader,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'User-Agent': 'PostmanRuntime/7.35.0'
+            }
+        });
+
+        console.log(`🔧 Alterando usuario ID: ${id}...`);
+        
+        world.response = await cineflixContext.patch(`${endpoint}`, {
+            data: payload
+        });
+
+        return world.response;
+    }
+
+     async validarCamposUsuarios(world: any, dataTable: any) {
+        const body = await world.response.json();
+        const usuarios = body.content;
+        const camposEsperados = dataTable.raw().flat();
+
+        expect(Array.isArray(usuarios), "O campo 'content' não é um array").toBe(true);
+
+        usuarios.forEach((usuario: any) => {
+            console.log(`Validando campos do usuário: ${usuario.name}`);
+            RestUtils.validateFields(usuario, camposEsperados);
+
+            expect(usuario.department).toHaveProperty('id');
+            expect(usuario.department).toHaveProperty('name');
+            expect(usuario.accessProfile).toHaveProperty('title');
+        });
+
+        console.log(`✅ Todos os ${usuarios.length} usuários possuem os campos: ${camposEsperados.join(', ')}`);
+    }
 }
